bc9bb22451205cdba49e674c6b0070115dac16b8
Levi-josh <levijoshuakelly@gmail.com>
2024-01-31 10:31:29 -0200

    updated

--- src/Delcart.js
+++ src/Delcart.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const route = express.Router()
 const maincart = require('./Newcartsheme')
-
+const users = require('./Signupschema')
 
 
 route.route('/:id').delete(async (req, res) => {
     try {
-        const filter = await maincart.deleteOne({ _id: req.params.id })
+        const filter = await users.findOne({ 'items._id': req.params.id })
+        const filtered = filter.items.filter(prev => prev._id == req.params.id)
+
+        const filter1 = await users.updateOne({ _id: filter._id }, { $pull: { items: filtered[0] } })
+        console.log(filter1)
         res.status(201).json({ 'message': 'deleted' })
 
+
     } catch (err) {
+        console.log(err.message)
         res.status(500).json({ 'error': err })
 
     }
