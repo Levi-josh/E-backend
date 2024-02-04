62eae553d4a9937a84e7799d34d561e1e0ab5c6c
Levi-josh <levijoshuakelly@gmail.com>
2024-02-04 20:42:50 -0200

    updated

--- src/Complete.js
+++ src/Complete.js
@@ -18,14 +18,17 @@ route.route('/:id/:id1').put(async (req, res) => {
 
     try {
 
+
+
         const myuser = await user.findOne({ 'items._id': req.params.id })
         const cart = myuser.items.filter(prev => prev._id == req.params.id)
         const result = await user.updateOne({ _id: req.params.id1 }, { $push: { favorites: cart[0] } })
-        const result1 = await user.updateOne({ 'items._id': req.params.id }, { $set: { 'items.$.selected': true } })
-        res.status(200).json(result1)
+        const filter1 = await user.updateOne({ _id: req.params.id1 }, { $pull: { items: cart[0] } })
+        console.log(filter1)
+        res.status(200).json(result)
 
     } catch (err) {
-        console.log(err.message)
+
         const showerror = error(err)
         res.status(500).json(showerror)
     }
