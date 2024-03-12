aa302a5403870245840a229e16c40676e611a5fd
Levi-josh <levijoshuakelly@gmail.com>
2024-03-12 19:57:45 -0200

    updated

--- src/controller/Pushnote.js
+++ src/controller/Pushnote.js
@@ -1,5 +1,5 @@
 const webPush = require('web-push');
-
+const users = require("../models/Signupschema");
 const subscriptions = new Map();
 
 const subscribe = async(req, res) => {
@@ -12,15 +12,15 @@ const subscribe = async(req, res) => {
 const sendnote = async(req, res) => {
   const { userId } = req.params;
   const subscription = subscriptions.get(userId);
-
   if (!subscription) {
     return res.status(404).json({ error: 'User not found' });
   }
-
+  const user = await users.findOne({_id:userId})
+  const message = `Hi ${user.username},welcome to Glamour Grove one of the best e-commerce shopping app which offers your the best services at a discount rate,do well to read more about as in our about page. `
   const notificationPayload = {
     notification: {
       title: 'Push Notification',
-      body: 'This is a push notification for you!',
+      body: message,
       icon:'https://img.freepik.com/free-photo/lot-different-clothes-hanging-wardrobe_181624-16122.jpg?size=626&ext=jpg&ga=GA1.1.103364066.1699032278&semt=sph'
     },
   };
