6c485310d7e61747ef3e5bd0a4b46ab2bd05a504
Levi-josh <levijoshuakelly@gmail.com>
2024-03-13 20:34:26 -0200

    updated

--- src/controller/Pushnote.js
+++ src/controller/Pushnote.js
@@ -19,6 +19,7 @@ const sendnote = async(req, res) => {
   const message = `Hi ${user.username},welcome to Glamour Grove one of the best e-commerce shopping app which offers your the best services at a discount rate,do well to read more about as in our about page. `
   const notificationPayload = {
     notification: {
+      type: "type1",
       title: 'Hey there',
       body: message,
       icon:'https://img.freepik.com/free-photo/lot-different-clothes-hanging-wardrobe_181624-16122.jpg?size=626&ext=jpg&ga=GA1.1.103364066.1699032278&semt=sph'
@@ -38,11 +39,10 @@ const sendnote2 = async(req, res) => {
   if (!subscription) {
     return res.status(404).json({ error: 'User not found' });
   }
-  const date = new Date()
-  const newtime = date.toLocaleTimeString()
   const message =`Your cart has been received and your goods are expected to arrive in your location in the next three days.please do not forget to rate our services even though we dont have that feature yet lol.`
   const notificationPayload = {
     notification: {
+      type: "type2",
       title: 'Order received',
       body: message,
       icon:'https://img.freepik.com/free-photo/lot-different-clothes-hanging-wardrobe_181624-16122.jpg?size=626&ext=jpg&ga=GA1.1.103364066.1699032278&semt=sph'
