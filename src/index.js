ea65e7d8e7e42958a487aa1871b470a2217c9396
Levi-josh <levijoshuakelly@gmail.com>
2025-03-22 07:15:31 -0200

    updated

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const cookie = require('cookie-parser');
 const bodyparser= require('body-parser');
 const errorhandler = require("./middleware/error")
 const routes = require('./Routes/request');
+const compression = require("compression");
 const port = process.env.port || 3500;
 webpush.setVapidDetails(
     'mailto:levijoshuakelly@gmail.com', // your email address
@@ -15,6 +16,15 @@ webpush.setVapidDetails(
     process.env.Private_key
 );
 App.use(cors({origin:['http://localhost:3000','https://my-commerce.onrender.com']}))
+app.use(compression({
+    threshold: 1024, // Compress responses > 1KB
+    filter: (req, res) => {
+      if (req.headers['x-no-compression']) {
+        return false; // Skip if client asks not to compress
+      }
+      return compression.filter(req, res);
+    }
+  }));
 App.use(express.json())
 App.use(cookie())
 App.use(bodyparser.json())
